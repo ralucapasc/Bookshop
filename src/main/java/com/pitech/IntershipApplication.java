654b972aea1bf5ea1afa85d639c2a1c531abc78b
ralucapasc <raluca_pasc@yahoo.com>
2019-01-22 10:05:15 +0200

    Create IntershipApplication.java

---
+++ src/main/java/com/pitech/IntershipApplication.java
@@ -0,0 +1,15 @@
+package com.pitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@SpringBootApplication
+public class InternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApplication.class, args);
+	}
+
+}
